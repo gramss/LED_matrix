9b50ec34181f3610b43695406434d8c6ae4ea886
Flo <flo.gramss@gmail.com>
2017-02-02 10:54:05 +0100

    changed baud rate, fixed showGraph duplicate

--- Siemens_LED_Matrix.ino
+++ Siemens_LED_Matrix.ino
@@ -23,7 +23,7 @@ const uint16_t colors[] = {
 
 void setup() {
   // initialize serial:
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   matrix.begin();
   matrix.setTextWrap(false);
@@ -74,7 +74,6 @@ void drawGraph(){
   if(Serial.available() > 0){
     int stripe_nr = Serial.parseInt();  // get Stripe Number from serial line
     int height = map(Serial.parseInt(), 0, 100, 0, HIGHT_MATRIX); // Map to height 
-    matrix.Color(Serial.parseInt(), Serial.parseInt(), Serial.parseInt() ); // get color from Serial line
 
     matrix.drawLine(stripe_nr, 0, stripe_nr, height, matrix.Color(Serial.parseInt(), Serial.parseInt(), Serial.parseInt() ));
   }
